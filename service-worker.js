cf8d4cb5fe15c249fb5afe71df8cf3f8069c0117
kpmike87 <mrizzo125@gmail.com>
2026-07-27 21:51:19 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "circuit-timer-v50";
+const CACHE_NAME = "circuit-timer-v51";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=50",
-  "./app.js?v=50",
+  "./styles.css?v=51",
+  "./app.js?v=51",
   "./manifest.webmanifest",
   "./icon.svg",
 ];
